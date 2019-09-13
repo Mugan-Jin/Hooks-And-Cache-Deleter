72ff54fc9ee5d43a5a4391b7506d3622ef0f6744
nate <nds1372@rit.edu>
2019-09-13 14:09:40 -0400

    Added copy and paste

--- src/deleter/ConsoleController.java
+++ src/deleter/ConsoleController.java
@@ -3,10 +3,16 @@ package deleter;
 import java.io.PrintStream;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.stream.Collectors;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ContextMenu;
 import javafx.scene.control.ListView;
+import javafx.scene.control.MenuItem;
+import javafx.scene.input.Clipboard;
+import javafx.scene.input.ClipboardContent;
+import javafx.scene.input.DataFormat;
 
 public class ConsoleController implements Initializable {
 	
@@ -18,6 +24,23 @@ public class ConsoleController implements Initializable {
 		final PrintStream ps = new PrintStream(new Console(this.console), false);
 		System.setOut(ps);
 		System.setErr(ps);
+		final ContextMenu cm = new ContextMenu();
+		final MenuItem copy = new MenuItem("Copy Selected");
+		copy.setOnAction(e -> {
+			final String s = this.console.getSelectionModel().getSelectedItems().stream().collect(Collectors.joining(System.lineSeparator()));
+			final ClipboardContent content = new ClipboardContent();
+			content.put(DataFormat.PLAIN_TEXT, s);
+			Clipboard.getSystemClipboard().setContent(content);
+		});
+		final MenuItem copyAll = new MenuItem("Copy All");
+		copyAll.setOnAction(e -> {
+			final String s = this.console.getItems().stream().collect(Collectors.joining(System.lineSeparator()));
+			final ClipboardContent content = new ClipboardContent();
+			content.put(DataFormat.PLAIN_TEXT, s);
+			Clipboard.getSystemClipboard().setContent(content);
+		});
+		cm.getItems().addAll(copy, copyAll);
+		this.console.setContextMenu(cm);
 	}
 
 }
